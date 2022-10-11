b7eac820ec9c2328ca8fbf0bc8f91634d314596a
W.H.P.Fernando <95269066+Hima-K@users.noreply.github.com>
2022-10-11 18:04:58 +0530

    Add files via upload

---
+++  Python program to print odd numbers in a List.py
@@ -0,0 +1,19 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2638
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 # Python program to print odd Numbers in a List\
+\
+# list of numbers\
+list1 = [10, 21, 4, 45, 66, 93]\
+\
+# iterating each number in list\
+for num in list1:\
+\
+	# checking condition\
+	if num % 2 != 0:\
+	print(num, end=" ")\
+}
